9cc642bc24704f8e0df0b9d3cc9cfaf90ccc1937
Jayanth P <jayanthjayram6968@gmail.com>
2024-12-23 15:14:03 +0530

    bordercolorchanged

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -10,7 +10,7 @@ const Balance = () => {
     <div className="flex items-center justify-center w-full h-full mt-12">
         <div className="flex justify-start relative flex-col gap-7 items-start rounded-xl w-[500px] h-[300px] bg-[#D3E3FF]">
           <p className="mt-2 ml-4 text-2xl font-semibold">Supplier Balance</p>
-          <div className="flex ml-3 flex-col justify-start gap-6 items-start border border-[#D5D7DA] w-[80%] h-[6rem] rounded-md bg-white">
+          <div className="flex ml-3 flex-col justify-start gap-6 items-start border border-[#C3C3C3] w-[80%] h-[6rem] rounded-md bg-white">
             <p className="mt-2 ml-2 font-medium">Total Balance</p>
             <p className="ml-2 font-medium">₹2500000.00</p>
           </div>
